2b541eda842e189d51a5141fca33cae82d4004b9
mazizhen <ma.zzh@vivebest.com>
2023-02-27 16:20:13 +0800

    模块搭建

---
+++ order/src/test/java/com/metazz/metamall/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metazz.metamall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ product/src/test/java/com/metazz/metamall/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metazz.metamall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
