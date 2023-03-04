08e07e5765feabac2c1fc6aa81675c83d4bb9df1
mazizhen <ma.zzh@vivebest.com>
2023-03-04 19:20:54 +0800

    gateway作为网关简单示例

---
+++ gateway/src/test/java/com/metazz/metamall/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.metazz.metamall.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
