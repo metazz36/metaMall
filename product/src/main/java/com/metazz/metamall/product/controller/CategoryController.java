7481ced9b4d5db140bd49d2f2a949db6e95e28b1
mazizhen <ma.zzh@vivebest.com>
2023-03-04 22:42:32 +0800

    通过递归获取产品分类树形结构

--- product/src/main/java/com/metazz/metamall/product/controller/CategoryController.java
+++ product/src/main/java/com/metazz/metamall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.metazz.metamall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,15 +31,15 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    /**
-     * 列表
-     */
-    @RequestMapping("/list")
-    //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
 
-        return R.ok().put("page", page);
+    /**
+     * 查出所有分类以及分类，以树形结构组装起来
+     */
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities =  categoryService.listWithTree();
+
+        return R.ok().put("data", entities);
     }
 
 
